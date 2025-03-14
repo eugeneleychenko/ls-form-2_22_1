35074c1c11fda40bd0876f5dd32e5042b566b705
eugeneleychenko <nycspicebo@gmail.com>
2025-03-14 16:19:03 -0400

    Replace Lead ID dropdown with text input for improved user experience in Basic Information section

--- eugene/insurance-form/components/form-sections/basic-information.tsx
+++ eugene/insurance-form/components/form-sections/basic-information.tsx
@@ -17,28 +17,14 @@ export default function BasicInformation() {
             <FormLabel required>
               Lead ID * <sub className="text-xs text-gray-500">[Airtable: Lead ID]</sub>
             </FormLabel>
-            <Select onValueChange={field.onChange} defaultValue={field.value}>
-              <FormControl>
-                <SelectTrigger className="border-gray-300 focus:border-primary focus:ring-primary">
-                  <SelectValue placeholder="Select Lead ID (Required)" />
-                </SelectTrigger>
-              </FormControl>
-              <SelectContent>
-                <SelectItem value="HULK">HULK</SelectItem>
-                <SelectItem value="White Data">White Data</SelectItem>
-                <SelectItem value="Power Rangers">Power Rangers</SelectItem>
-                <SelectItem value="Orange Data">Orange Data</SelectItem>
-                <SelectItem value="Glorias">Glorias</SelectItem>
-                <SelectItem value="Power Rangers Inbound">Power Rangers Inbound</SelectItem>
-                <SelectItem value="Fire Inbound">Fire Inbound</SelectItem>
-                <SelectItem value="White inbound">White inbound</SelectItem>
-                <SelectItem value="Greens">Greens</SelectItem>
-                <SelectItem value="Red LT">Red LT</SelectItem>
-                <SelectItem value="Orange Inbound">Orange Inbound</SelectItem>
-                <SelectItem value="Yellow">Yellow</SelectItem>
-                <SelectItem value="Spiderman X">Spiderman X</SelectItem>
-              </SelectContent>
-            </Select>
+            <FormControl>
+              <Input
+                type="text"
+                placeholder="Enter Lead ID (Required)"
+                {...field}
+                className="border-gray-300 focus:border-primary focus:ring-primary"
+              />
+            </FormControl>
             <FormMessage className="text-destructive" />
           </FormItem>
         )}
@@ -136,9 +122,19 @@ export default function BasicInformation() {
                 </SelectTrigger>
               </FormControl>
               <SelectContent>
-                <SelectItem value="website">Website</SelectItem>
-                <SelectItem value="referral">Referral</SelectItem>
-                <SelectItem value="advertisement">Advertisement</SelectItem>
+                <SelectItem value="HULK">HULK</SelectItem>
+                <SelectItem value="White Data">White Data</SelectItem>
+                <SelectItem value="Power Rangers">Power Rangers</SelectItem>
+                <SelectItem value="Orange Data">Orange Data</SelectItem>
+                <SelectItem value="Glorias">Glorias</SelectItem>
+                <SelectItem value="Power Rangers Inbound">Power Rangers Inbound</SelectItem>
+                <SelectItem value="Fire Inbound">Fire Inbound</SelectItem>
+                <SelectItem value="White inbound">White inbound</SelectItem>
+                <SelectItem value="Greens">Greens</SelectItem>
+                <SelectItem value="Red LT">Red LT</SelectItem>
+                <SelectItem value="Orange Inbound">Orange Inbound</SelectItem>
+                <SelectItem value="Yellow">Yellow</SelectItem>
+                <SelectItem value="Spiderman X">Spiderman X</SelectItem>
               </SelectContent>
             </Select>
             <FormMessage className="text-destructive" />
